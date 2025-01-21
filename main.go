ef76b0d008a706551cd91161477406f22f20ca39
Rapha <rapphha@gmail.com>
2025-01-21 15:52:57 -0700

    feat: Add goroutines example with random sleep durations and improve code comments
    - Added an example demonstrating the use of goroutines for concurrent execution.
    - Each goroutine simulates work by printing a message and sleeping for a random duration.
    - Added detailed comments to explain the purpose of `go` keyword, random delays, and concurrent behavior.
    - Improved readability by structuring the code with meaningful comments.

--- main.go
+++ main.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
+func hello(msg string) {
+	fmt.Println(msg + "- goroutine")
+	time.Sleep(time.Duration(rand.Intn(1000)) * time.Second)
+}
+
 func main() {
+	go hello("Hello 1")
+	go hello("Hello 2")
 	time.Sleep(1 * time.Second)
 	fmt.Println("Common Callback ")
 }
